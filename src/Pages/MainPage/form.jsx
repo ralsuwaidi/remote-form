2a36b04980d1c4d583d21d0dfaa98981bdbc0630
rashed <x.suwaidi@gmail.com>
2023-02-27 16:47:02 +0400

    change placeholder

--- src/Pages/MainPage/form.jsx
+++ src/Pages/MainPage/form.jsx
@@ -108,7 +108,7 @@ const Form = () => {
                                                 onChange={e => setName(e.target.value)}
                                                 id="fullname"
                                                 className="block w-full flex-1  rounded-md border-gray-300 focus:border-gray-500 focus:ring-gray-500 sm:text-sm"
-                                                placeholder="Rashed AlSuwaidi"
+                                                placeholder="Musab AlSuwaidi"
                                             />
                                         </div>
                                         <p class="mt-2 text-sm text-red-600 dark:text-red-500">{errorResponse.fullname}</p>
